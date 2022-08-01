[FIX, ADD][main, outputs] Fix calculation and check consistency of the results (output folder)

df_types/InputDataPool.py
@@ -1,9 +1,10 @@
 import pandera
+import numpy as np
 
 
 class InputDataPool_schema(pandera.SchemaModel):
-    id_pool: pandera.typing.Series[int]
-    spread: pandera.typing.Series[float]
+    id_pool: pandera.typing.Index[int]
+    spread: pandera.typing.Series[np.float64]
 
 
 InputDataPool = pandera.typing.DataFrame[InputDataPool_schema]

df_types/PolDF.py
@@ -5,9 +5,9 @@ import numpy as np
 class Pol_schema(pandera.SchemaModel):
     id: pandera.typing.Index[int]
     id_pool: pandera.typing.Series[int]
-    math_res_opening: pandera.typing.Series[np.float32]
-    math_res_before_ps: pandera.typing.Series[np.float32]
-    math_res_closing: pandera.typing.Series[np.float32]
+    math_res_opening: pandera.typing.Series[np.float]
+    math_res_bef_ps: pandera.typing.Series[np.float]
+    math_res_closing: pandera.typing.Series[np.float]
 
 
 PolDF = pandera.typing.DataFrame[Pol_schema]

df_types/PoolDF.py
@@ -4,9 +4,9 @@ import numpy as np
 
 class Pool_schema(pandera.SchemaModel):
     id_pool: pandera.typing.Index[int]
-    math_res_opening: pandera.typing.Series[np.float32]
-    math_res_before_ps: pandera.typing.Series[np.float32]
-    math_res_closing: pandera.typing.Series[np.float32]
+    math_res_opening: pandera.typing.Series[np.float64]
+    math_res_bef_ps: pandera.typing.Series[np.float64]
+    math_res_closing: pandera.typing.Series[np.float64]
     spread: pandera.typing.Series[np.float64]  # FIXME : unify float types
 
 